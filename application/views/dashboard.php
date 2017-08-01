4ab8e51bdb2a4d00df8f56a76b497e4487e4a4c3
Miko <mikohendroc@gmail.com>
2017-08-01 16:49:02 +0700

    123

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,210 +1,129 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | General Form Elements</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/');?>bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/');?>bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/');?>dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/');?>dist/css/skins/_all-skins.min.css">
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
-<body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
-  <!--header-->
-  <?php echo "$header";?>
-  <!--end header-->
-  <!-- Left side column. contains the logo and sidebar -->
-  <!--sidebar-->
-  <?php echo "$sidebar";?>
-  <!--end sidebar-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>AdminLTE 2 | General Form Elements</title>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>bower_components/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>dist/css/AdminLTE.min.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>dist/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+            <?php echo "$header"; ?>
+            <?php echo "$sidebar"; ?>
+            <div class="content-wrapper">
+                <section class="content-header">
+                    <h1>
+                        General Form Elements
+                        <small>Preview</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                        <li><a href="#">Forms</a></li>
+                        <li class="active">General Elements</li>
+                    </ol>
+                </section>
+                <section class="content">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="box box-primary">
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        General Form Elements
-        <small>Preview</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Forms</a></li>
-        <li class="active">General Elements</li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <!-- left column -->
-        <div class="col-md-12">
-          <!-- general form elements -->
-          <div class="box box-primary">
-            <div class="box-header with-border">
-              <h3 class="box-title">Input Data (Kategory & bulan)</h3>
+                                <form role="form">
+                                    <div class="box-body">
+                                        <div class="row">
+                                            <div class="form-group">
+                                                <div class="col-sm-6">
+                                                    <label for="exampleInputEmail1">Judul</label>
+                                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Judul">
+                                                </div>
+                                                <div class="col-sm-6"> 
+                                                    <label for="exampleInputPassword1">Tahun</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <hr/>
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Januari</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Februari</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Maret</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">April</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Mei</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Juni</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Juli</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Agustus</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">September</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Oktober</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Nopember</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Desember</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-footer">
+                                        <button type="submit" class="btn btn-primary col-sm-12">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-            <!-- /.box-header -->
-            <!-- form start -->
-            <form role="form">
-              <div class="box-body">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Judul</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Judul">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Tahun</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Tahun">
-                </div>
-              </div>
-              <!-- /.box-body -->
-
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </div>
-            </form>
-          </div>
-          <!-- /.box -->
+            <!--footer-->
+            <?php echo "$footer"; ?>
+            <!--end footer-->
         </div>
-        <!--/.col (left) -->
-        <!-- right column -->
-<!--        <div class="col-md-6">
-           general form elements disabled 
-          <div class="box box-warning">
-            <div class="box-header with-border">
-              <h3 class="box-title">General Elements</h3>
-            </div>
-             /.box-header 
-            <div class="box-body">
-              <form role="form">
-                 checkbox 
-                <div class="form-group">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox">
-                      Checkbox 1
-                    </label>
-                  </div>
 
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox">
-                      Checkbox 2
-                    </label>
-                  </div>
-
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox" disabled>
-                      Checkbox disabled
-                    </label>
-                  </div>
-                </div>
-
-                 radio 
-                <div class="form-group">
-                  <div class="radio">
-                    <label>
-                      <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                      Option one is this and that&mdash;be sure to include why it's great
-                    </label>
-                  </div>
-                  <div class="radio">
-                    <label>
-                      <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                      Option two can be something else and selecting it will deselect option one
-                    </label>
-                  </div>
-                  <div class="radio">
-                    <label>
-                      <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled>
-                      Option three is disabled
-                    </label>
-                  </div>
-                </div>
-
-                 select 
-                <div class="form-group">
-                  <label>Select</label>
-                  <select class="form-control">
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label>Select Disabled</label>
-                  <select class="form-control" disabled>
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                  </select>
-                </div>
-
-                 Select multiple
-                <div class="form-group">
-                  <label>Select Multiple</label>
-                  <select multiple class="form-control">
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label>Select Multiple Disabled</label>
-                  <select multiple class="form-control" disabled>
-                    <option>option 1</option>
-                    <option>option 2</option>
-                    <option>option 3</option>
-                    <option>option 4</option>
-                    <option>option 5</option>
-                  </select>
-                </div>
-
-              </form>
-            </div>
-             /.box-body 
-          </div>
-           /.box 
-        </div>-->
-        <!--/.col (right) -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  
-  <!--footer-->
-  <?php echo "$footer";?>
-  <!--end footer-->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery 3 -->
-<script src="<?php echo base_url('assets/');?>bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?php echo base_url('assets/');?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- FastClick -->
-<script src="<?php echo base_url('assets/');?>bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="<?php echo base_url('assets/');?>dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url('assets/');?>dist/js/demo.js"></script>
-</body>
+        <script src="<?php echo base_url('assets/'); ?>bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="<?php echo base_url('assets/'); ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url('assets/'); ?>bower_components/fastclick/lib/fastclick.js"></script>
+        <script src="<?php echo base_url('assets/'); ?>dist/js/adminlte.min.js"></script>
+        <script src="<?php echo base_url('assets/'); ?>dist/js/demo.js"></script>
+    </body>
 </html>
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
+      <!--<b>Version</b> 2.4.0-->
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; @@ <a href="https://adminlte.io"></a>.</strong> All rights reserved.
   </footer>
