2e97eb6d23a922c5e259c0ba04ff5ce246cda6b8
Miko <mikohendroc@gmail.com>
2017-08-04 16:50:56 +0700

    123

--- application/controllers/ProcessController.php
+++ application/controllers/ProcessController.php
@@ -18,48 +18,85 @@ class ProcessController extends CI_Controller {
     }
 
     public function getData() {
-        
-        $tmpfname = './uploads/data__skripsi_2014.xlsx';
-            if(file_exists($tmpfname)){
-                echo "file ada";
-                $excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
-		$excelObj = $excelReader->load($tmpfname);
-		$worksheet = $excelObj->getSheet(0);
-		$lastRow = $worksheet->getHighestRow();
-		
-		echo "<table>";
-		for ($row = 1; $row <= $lastRow; $row++) {
-			 echo "<tr><td>";
-			 echo $worksheet->getCell('B'.$row)->getValue();
-			 echo "</td><tr>";
-		}
-		echo "</table>";	
-            }else{
-                echo "file tidak ada";
+        $pasal = $this->input->post("pasal");
+        $tahun = $this->input->post("tahun");
+        $pertama = $_FILES['pertama']['name'];
+        $extPertama = pathinfo($pertama, PATHINFO_EXTENSION);
+        $kedua = $_FILES['kedua']['name'];
+        $extKedua = pathinfo($kedua, PATHINFO_EXTENSION);
+
+        if ($pasal == "") {
+            echo json_encode(array("error" => "1", "message" => "pasal harus diisi"));
+        } else if ($tahun == "") {
+            echo json_encode(array("error" => "1", "message" => "tahun harus diisi"));
+        } else if ($pertama == null) {
+            echo json_encode(array("error" => "1", "message" => "file pertama harus diisi atau file pertama harus excel"));
+        } else if ($kedua == null) {
+            echo json_encode(array("error" => "1", "message" => "file pertama harus diisi atau file pertama harus excel"));
+        } else {
+            $array1 = array();
+            $array2 = array();
+            $file1 = $_FILES['pertama']['tmp_name'];
+            $file2 = $_FILES['kedua']['tmp_name'];
+            if (file_exists($file1) && file_exists($file2)) {
+                $excelReader = PHPExcel_IOFactory::createReaderForFile($file1);
+                $excelObj = $excelReader->load($file1);
+                $worksheet = $excelObj->getSheet(0);
+                $lastRow = $worksheet->getHighestRow();
+                for ($row = 2; $row <= $lastRow; $row++) {
+                    array_push($array1, $worksheet->getCell('B' . $row)->getValue());
+                }
+                
+                $excelReader = PHPExcel_IOFactory::createReaderForFile($file2);
+                $excelObj = $excelReader->load($file2);
+                $worksheet = $excelObj->getSheet(0);
+                $lastRow = $worksheet->getHighestRow();
+                for ($row = 2; $row <= $lastRow; $row++) {
+                    array_push($array2, $worksheet->getCell('B' . $row)->getValue());
+                }
+                $input = array(
+                    "error" => 0,
+                    "file1" => $array1,
+                    "file2" => $array2
+                );
+                echo json_encode($input);
+            } else {
+                echo 'tidak ada file';
             }
+        }
         
+//        function
+
+//        echo $pasal.$tahun;
+////        untuk upload file
 //        $config['upload_path'] = './uploads/';
 //        $config['allowed_types'] = 'xls|xlsx';
-//        
-//
+//        $config['file_ext_tolower'] = 'xls';
+//        $config['file_name'] = uniqid();
+////        config upload file
 //        $this->load->library('upload', $config);
-//
-//// Alternately you can set preferences by calling the ``initialize()`` method. Useful if you auto-load the class:
+////        Alternately you can set preferences by calling the ``initialize()`` method. Useful if you auto-load the class:
 //        $this->upload->initialize($config);
-//
-//        if (!$this->upload->do_upload('tahun1')) {
-////            $error = array('error' => $this->upload->display_errors());
-////
-////            $this->load->view('upload_form', $error);
-//            echo $this->upload->display_errors();
-//            echo '0';
+//        if (!$this->upload->do_upload('excel')) {
+//            echo 0;
 //        } else {
-////            $data = array('upload_data' => $this->upload->data());
-////
-////            $this->load->view('upload_success', $data);
-//            echo '1';
-//            
-//            
+//            $tmpfname = "./uploads/" . $config['file_name'] . ".xlsx";
+//            if (file_exists($tmpfname)) {
+//                $excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+//                $excelObj = $excelReader->load($tmpfname);
+//                $worksheet = $excelObj->getSheet(0);
+//                $lastRow = $worksheet->getHighestRow();
+//
+//                echo "<table>";
+//                for ($row = 1; $row <= $lastRow; $row++) {
+//                    echo "<tr><td>";
+//                    echo $worksheet->getCell('B' . $row)->getValue();
+//                    echo "</td><tr>";
+//                }
+//                echo "</table>";
+//            }else{
+//                echo 'tidak ada file';
+//            }
 //        }
     }
 
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -31,44 +31,172 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="box box-primary">
-                                <?php
-                                echo form_open_multipart('proses/input');
-                                ?>
                                 <div class="box-body">
                                     <div class="row">
                                         <div class="form-group">
-                                            <div class="col-sm-12">
-                                                <label for="exampleInputEmail1">Judul</label>
-                                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Masukkan Judul">
+                                            <div class="col-sm-6">
+                                                <label for="exampleInputEmail1">Pasal Pelanggaran</label>
+                                                <input type="text" class="form-control" id="pasal" placeholder="Masukkan Pasal Pelanggaran" name="pasal" value="287">
                                             </div>
-
-                                            <div class="col-sm-6"> 
-                                                <label for="exampleInputPassword1">Tahun 1</label>
-                                                <input type="file" class="form-control" id="tahun1" class="form-control" name="tahun1">
+                                            <div class="col-sm-6">
+                                                <label for="exampleInputEmail1">Tahun Pelanggaran</label>
+                                                <input type="text" class="form-control" id="tahun" placeholder="Masukkan Tahun Actual Pelanggaran" name="tahun" value="2014">
                                             </div>
-                                            
-                                            <div class="col-sm-6"> 
-                                                <label for="exampleInputPassword1">Tahun 1</label>
-                                                <input type="file" class="form-control" id="tahun1" class="form-control" name="tahun2">
+                                            <div class="col-sm-6">
+                                                <label for="exampleInputEmail1">Excel Tahun Pertama</label>
+                                                <input type="file" class="form-control" id="pertama" name="pertama">
                                             </div>
-
+                                            <div class="col-sm-6">
+                                                <label for="exampleInputEmail1">Excel Tahun Kedua</label>
+                                                <input type="file" class="form-control" id="kedua" name="kedua">
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <label for="exampleInputEmail1">&nbsp;</label>
+                                                <button type="button" class="btn btn-primary" onclick="lihatData();">Lihat Data</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="box-footer">
-                                    <button type="submit" class="btn btn-primary col-sm-12">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="box box-primary">
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <table class="table table-striped table-bordered" id="table-1">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Bulan</th>
+                                                        <th>Tahun</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <th>January</th>
+                                                        <th id="one-1"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>February</th>
+                                                        <th id="one-2"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Maret</th>
+                                                        <th id="one-3"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>April</th>
+                                                        <th id="one-4"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Mei</th>
+                                                        <th id="one-5"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Juni</th>
+                                                        <th id="one-6"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Juli</th>
+                                                        <th id="one-7"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Agustus</th>
+                                                        <th id="one-8"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>September</th>
+                                                        <th id="one-9"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Oktober</th>
+                                                        <th id="one-10"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>November</th>
+                                                        <th id="one-11"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Desember</th>
+                                                        <th id="one-12"></th>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <table class="table table-striped table-bordered" id="table-2">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Bulan</th>
+                                                        <th>Tahun</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <th>January</th>
+                                                        <th id="two-1"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>February</th>
+                                                        <th id="two-2"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Maret</th>
+                                                        <th id="two-3"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>April</th>
+                                                        <th id="two-4"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Mei</th>
+                                                        <th id="two-5"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Juni</th>
+                                                        <th id="two-6"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Juli</th>
+                                                        <th id="two-7"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Agustus</th>
+                                                        <th id="two-8"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>September</th>
+                                                        <th id="two-9"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Oktober</th>
+                                                        <th id="two-10"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>November</th>
+                                                        <th id="two-11"></th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Desember</th>
+                                                        <th id="two-12"></th>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <button type="button" class="btn btn-danger" onclick="prosesData();">Process</button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <?php
-                                echo form_close();
-                                ;
-                                ?>
                             </div>
                         </div>
                     </div>
                 </section>
             </div>
             <!--footer-->
-<?php echo "$footer"; ?>
+            <?php echo "$footer"; ?>
             <!--end footer-->
         </div>
 
@@ -77,5 +205,55 @@
         <script src="<?php echo base_url('assets/'); ?>bower_components/fastclick/lib/fastclick.js"></script>
         <script src="<?php echo base_url('assets/'); ?>dist/js/adminlte.min.js"></script>
         <script src="<?php echo base_url('assets/'); ?>dist/js/demo.js"></script>
+        <script>
+                                                function lihatData() {
+                                                    var pasal = $('#pasal').val();
+                                                    var tahun = $('#tahun').val();
+                                                    var pertama = $('#pertama')[0].files[0];
+                                                    var kedua = $('#kedua')[0].files[0];
+                                                    var formData = new FormData();
+                                                    formData.append("pasal", pasal);
+                                                    formData.append("tahun", tahun);
+                                                    formData.append("pertama", pertama);
+                                                    formData.append("kedua", kedua);
+                                                    $.ajax({
+                                                        url: "<?php echo base_url(); ?>proses/input",
+                                                        type: 'POST',
+                                                        dataType: 'json',
+                                                        data: formData,
+                                                        async: false,
+                                                        cache: false,
+                                                        contentType: false,
+                                                        processData: false,
+                                                        beforeSend: function (xhr) {
+//                                                                $('#table-1').find("tbody").empty();
+//                                                                $('#table-2').find("tbody").empty();
+                                                        },
+                                                        success: function (response, textStatus, jqXHR) {
+                                                            if (response.error == 1) {
+                                                                alert(response.error);
+                                                            } else {
+                                                                for (var i = 0; i < response.file1.length; i++) {
+                                                                    $('#one-' + (i + 1)).text(response.file1[i]);
+                                                                }
+                                                                for (var i = 0; i < response.file2.length; i++) {
+                                                                    $('#two-' + (i + 1)).text(response.file2[i]);
+                                                                }
+                                                            }
+
+                                                        },
+                                                        complete: function (jqXHR, textStatus) {
+
+                                                        }
+                                                    });
+                                                }
+
+                                                function prosesData() {
+                                                alert("123");
+                                                    $('#table-1 tr:eq(2) td').each(function () {
+                                                        alert($(this).text());
+                                                    });
+                                                }
+        </script>
     </body>
 </html>
